5a3cf03acb1402f598b0db5fa8f8cbdfe2a4b3f3
Christopher Lovell <15553561+gmfCoding@users.noreply.github.com>
2023-11-12 01:01:06 +1030

    Begun work on volume slider

---
+++ Assets/Scripts/Menu/EscMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EscMenu : MonoBehaviour
+{
+
+    public GameObject menu;
+    bool status => menu.activeInHierarchy;
+
+    public Slider volume;
+
+    public void Awake()
+    {
+        if (PlayerPrefs.HasKey("volume"))
+            volume.value = PlayerPrefs.GetFloat("volume");
+        else
+            PlayerPrefs.SetFloat("volume", volume.value);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            menu.SetActive(!status);
+    }
+
+    public void OnVolumeChanged()
+    {
+        AudioListener.volume = volume.value;
+        PlayerPrefs.SetFloat("volume", volume.value);
+    }
+}
