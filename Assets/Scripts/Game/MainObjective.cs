3ccbd94506b253ac816aeccbd1e0f9798d5a67f8
Christopher Lovell <15553561+gmfCoding@users.noreply.github.com>
2023-11-12 12:55:44 +1030

    Add demon head mesh, for tokens? add main object --ive class

---
+++ Assets/Scripts/Game/MainObjective.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MainObjective : MonoBehaviour
+{
+    private int tokens;
+    public int winAmount;
+
+    RectTransform winPanel;
+    TMPro.TMP_Text tokenText;
+
+    bool winConditionRate;
+
+    bool hasWon;
+
+    public int Tokens { 
+        get => tokens;
+        set 
+        {
+            tokenText.text = value.ToString();
+            tokens = value; 
+        }
+    }
+    public float sampleTime = 20.0f;
+    public float time = 0.0f;
+
+    public void Update()
+    {
+        if (hasWon)
+            return;
+
+        if (winConditionRate)
+        {
+            time += Time.deltaTime;
+            if (time >= sampleTime)
+            {
+                time -= sampleTime;
+                if (Tokens >= winAmount)
+                    Win();
+                Tokens = 0;
+            }
+        }
+        else
+        {
+            if (Tokens >= winAmount)
+                Win();
+        }
+    }
+
+    public void Win()
+    {
+        winPanel.gameObject.SetActive(true);
+        hasWon = true;
+        // Disable look var, and enable mouse cursor to click continue button
+    }
+}
